a1e0af9c0e6657e6dfa32c70bb917c6ca04e9b2e
Andrew Preston <a.preston@sap.com>
2018-06-22 15:38:04 -0400

    Makes SimpleProgram.java an object oriented program

--- SimpleProgram.java
+++ SimpleProgram.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class SimpleProgram {
 	public static void main (String[] args) {
-		System.out.println("This program is simple.");
+		SimpleObject obj;
+		Scanner scan;
+		String choice;
+		String msg;
+
+		scan = new Scanner(System.in);
+		choice = "e";	//default value
+		msg = "";
+
+		System.out.print("Would you like to make a custom message? (y/n) ");
+		
+		while (!choice.equals("y") && !choice.equals("n")) {
+			choice = scan.next();
+		}
+
+		if (choice.equals("y")) {
+			System.out.println("Wow!\nWow!\nWow!");
+			System.out.println("Enter message: ");
+			scan.nextLine();
+			msg = scan.nextLine();
+			obj = new SimpleObject(msg);
+		}
+		
+		else
+			obj = new SimpleObject();
+
+		System.out.println(obj);
 	}
 }
